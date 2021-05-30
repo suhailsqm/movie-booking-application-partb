6602ed3d1d0224e61bee8a7694bc111c19cb0113
suhail <suhailsqm@gmail.com>
2021-05-30 11:09:36 +0530

    Creating header component

--- src/common/header/header.jsx
+++ src/common/header/header.jsx
@@ -1,32 +1,35 @@
 import react from "react";
 import "./header.css";
-
 import logo from "./logo.svg";
-
 import { Button } from "@material-ui/core";
+import {Link} from "react-router-dom"
 
 class Header extends react.Component {
   render() {
-    return (
-      <div className="app-header">
-        <img src={logo} alt="logo" className="app-logo"></img>
-        <div className="login-button">
-          <Button
-            variant="contained"
-            color="default"
-           
-          >
-            Login
-          </Button>
-        </div>
+      return (
+          <div className="app-header">
+              <img src={logo} alt="logo" className="app-logo"></img>
+              <div className="login-button">
+                  <Button
+                      variant="contained"
+                      color="default"
 
-        <div className="bookshow-button">
-          <Button variant="contained" color="primary">
-            Book Show
+                  >
+                      Login
           </Button>
-        </div>
-      </div>
-    );
+              </div>
+
+              {this.props.showBookShowButton === "true" ?
+                  <div className="bookshow-button">
+                      <Link to={"/bookshow/" + this.props.id}>
+                          <Button variant="contained" color="primary">
+                              Book Show
+                                </Button>
+                      </Link>
+                  </div>
+                  : ""}
+          </div>
+      );
   }
 }
 
